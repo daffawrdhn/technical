193175291bf2b48ceb050663ed249693164ffbe2
daffawrdhn <wardhanadty@gmail.com>
2022-08-24 03:23:19 +0700

    adding login register logout shop

--- signup.php
+++ signup.php
@@ -1,3 +1,46 @@
+<?php 
+ 
+include 'config.php';
+ 
+session_start();
+ 
+if (isset($_SESSION['email'])) {
+    header("Location: index.php");
+}
+ 
+if (isset($_POST['signup'])) {
+
+    $namadepan = $_POST['namadepan'];
+    $namatengah = $_POST['namatengah'];
+    $namabelakang = $_POST['namabelakang'];
+
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+ 
+    if ($password == $password) {
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $result = mysqli_query($conn, $sql);
+        if (!$result->num_rows > 0) {
+            $sql = "INSERT INTO users (email, nama_depan, nama_tengah, nama_belakang, password)
+                    VALUES ('$email', '$namadepan', '$namatengah', '$namabelakang', '$password')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                echo "<script>alert('Selamat, registrasi berhasil!')</script>";
+                header("Location: signin.php");
+            } else {
+                echo "<script>alert('Woops! Terjadi kesalahan.')</script>";
+            }
+        } else {
+            echo "<script>alert('Woops! Email Sudah Terdaftar.')</script>";
+        }
+         
+    } else {
+        echo "<script>alert('Password Tidak Sesuai')</script>";
+    }
+}
+ 
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,65 +53,7 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
 
-    
-
-    
-
 <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-    </style>
-
     
     <!-- Custom styles for this template -->
     <link href="assets/dist/css/signin.css" rel="stylesheet">
@@ -76,7 +61,7 @@
   <body class="text-center">
     
 <main class="form-signin w-100 m-auto">
-  <form>
+  <form method="POST" name="signup" onsubmit="return validateForm()" action="">
     <!-- <img class="mb-4" src="assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
     <svg xmlns="http://www.w3.org/2000/svg" width="72" height="57" class="me-2" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>
 
@@ -84,39 +69,58 @@
 
     <!-- nama depan -->
     <div class="form-floating form-top">
-      <input type="text" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="First name" title="No Space please">
+      <input type="text" name="namadepan" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="First name" title="No Space please">
       <label for="floatingInput">First name</label>
     </div>
 
     <!-- nama tengah -->
     <div class="form-floating form-mid">
-      <input type="text" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="Middle name" title="No Space please">
+      <input type="text" name="namatengah" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="Middle name" title="No Space please">
       <label for="floatingInput">Middle name</label>
     </div>
 
     <!-- nama belakang -->
     <div class="form-floating form-mid">
-      <input type="text" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="Last name" title="No Space please">
+      <input type="text" name="namabelakang" class="form-control" id="floatingInput" pattern="[a-zA-Z]+" placeholder="Last name" title="No Space please">
       <label for="floatingInput">Last name</label>
     </div>
 
     <!-- email as username -->
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
 
     <!-- password -->
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" pattern="(?=.*\d)(?=.*[#$@!%&*?])(?=.*[a-z])(?=.*[A-Z]).{8,16}" title="Must contain at least one number and one uppercase and lowercase letter, one special character, and at between 8 or 16 characters">
+      <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" pattern="(?=.*\d)(?=.*[#$@!%&*?])(?=.*[a-z])(?=.*[A-Z]).{8,16}" title="Must contain at least one number and one uppercase and lowercase letter, one special character, and at between 8 or 16 characters">
       <label for="floatingPassword">Password</label>
     </div>
 
-    <!-- password criteria check -->
+    <!-- input check -->
+    <script type="text/javascript">
+        function validateForm() {
+            var a = document.forms["signup"]["namadepan"].value;
+            var b = document.forms["signup"]["namatengah"].value;
+            var c = document.forms["signup"]["namabelakang"].value;
+            var d = document.forms["signup"]["email"].value;
+            var e = document.forms["signup"]["password"].value;
+            
+            if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "", e == null || e == "") {
+            alert("Please Fill All Required Field");
+            return false;
+            }
+        }
+    </script>
 
-    <button class="w-100 btn btn-lg btn-dark" type="submit">Sign Up</button>
+    <button class="w-100 btn btn-lg btn-dark" name="signup" type="submit">Sign Up</button>
+
+
     <p class="mt-5 mb-3 text-muted">Already have account?
       <a class="mt-5 mb-3 text-muted" href="signin.php"> Sign In</a>
+      <a class="mt-5 mb-3 text-muted"> | </a>
+      <a class="mt-5 mb-3 text-muted" href="index.php">Home</a>
+      
     </p>
     <p class="text-muted">&copy; 2017–2022</p>
   </form>
